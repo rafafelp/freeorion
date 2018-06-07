afa98a43eba1e0a892134329ef43172c412fe8bc
geoffthemedio <geoffthemedio@gmail.com>
2018-06-07 15:58:27 +0200

    WIP

--- Empire/Empire.cpp
+++ Empire/Empire.cpp
@@ -57,7 +57,8 @@ Empire::PolicyAdoptionInfo::PolicyAdoptionInfo(int turn, const std::string& cat,
 Empire::Empire() :
     m_authenticated(false),
     m_research_queue(m_id),
-    m_production_queue(m_id)
+    m_production_queue(m_id),
+    m_influence_queue(m_id)
 { Init(); }
 
 Empire::Empire(const std::string& name, const std::string& player_name,
@@ -68,7 +69,8 @@ Empire::Empire(const std::string& name, const std::string& player_name,
     m_authenticated(authenticated),
     m_color(color),
     m_research_queue(m_id),
-    m_production_queue(m_id)
+    m_production_queue(m_id),
+    m_influence_queue(m_id)
 {
     DebugLogger() << "Empire::Empire(" << name << ", " << player_name << ", " << empire_id << ", colour)";
     Init();
@@ -2388,8 +2390,9 @@ void Empire::CheckProductionProgress() {
     SetResourceStockpile(RE_INDUSTRY, m_production_queue.ExpectedNewStockpileAmount());
 }
 
-void Empire::CheckInfluenceProgress()
-{ m_resource_pools[RE_INFLUENCE]->SetStockpile(m_resource_pools[RE_INFLUENCE]->TotalAvailable()); }
+void Empire::CheckInfluenceProgress() {
+    m_resource_pools[RE_INFLUENCE]->SetStockpile(m_resource_pools[RE_INFLUENCE]->TotalAvailable());
+}
 
 void Empire::SetColor(const GG::Clr& color)
 { m_color = color; }
